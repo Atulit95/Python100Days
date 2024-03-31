b38eaef3542fc21894912b363783c9179ab652fa
Atulit95 <atulitgupta436@gmail.com>
2024-03-31 18:31:23 +0530

    env

---
+++ Day16 undestandind OOP.py
@@ -0,0 +1,13 @@
+from turtle import Turtle, Screen
+
+timmy = Turtle() # here timmy is object and Turtle is class used as blueprint
+print(timmy)
+
+my_screen=Screen()      # screen module shows the screen where or turtle will show
+print(my_screen.canvwidth)
+timmy.shape("turtle")
+timmy.color("red","green")
+timmy.forward(100)
+
+
+my_screen.exitonclick()  #allows the popup screen to close onclick
