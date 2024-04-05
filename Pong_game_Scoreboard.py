342d7212620145ffa6a2ede4fe36dbdc31aced81
Atulit95 <atulitgupta436@gmail.com>
2024-04-06 00:00:28 +0530

    day22

--- Pong_game_Scoreboard.py
+++ Pong_game_Scoreboard.py
@@ -12,13 +12,17 @@ class Scoreboard(Turtle):
         self.hideturtle()
         self.l_score = 0
         self.r_score = 0
-        self.goto(x_cor, y_cor)
+
+    def update_scoreboard(self):
+        self.goto(-100, 300)
+        self.write(self.l_score, align=ALIGN, font=FONT)
+        self.goto(-100, 300)
         self.write(self.l_score, align=ALIGN, font=FONT)
 
     def l_scored(self):
         self.l_score += 1
-        self.write(self.l_score, align=ALIGN, font=FONT)
+        self.update_scoreboard()
 
     def r_scored(self):
         self.r_score += 1
-        self.write(self.r_score, align=ALIGN, font=FONT)
+        self.update_scoreboard()
