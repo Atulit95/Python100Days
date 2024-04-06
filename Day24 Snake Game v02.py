08cfdb4aa08b4b3b3ff13ef5da96c21071a11af8
Atulit95 <atulitgupta436@gmail.com>
2024-04-06 22:37:47 +0530

    day24

---
+++ Day24 Snake Game v02.py
@@ -0,0 +1,57 @@
+# Here on day24 our goal is to add hoghscore in the game
+from turtle import Screen
+import time
+from snake import Snake
+from food import Food
+from SnakeGameScoreCard import Score
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("Snake Game  ----===e  o")
+screen.tracer(0)
+
+
+snake = Snake()
+food = Food()
+score = Score()
+
+screen.listen()
+screen.onkeypress(key="Up", fun=snake.turn_up)
+screen.onkeypress(key="Down", fun=snake.turn_down)
+screen.onkeypress(key="Left", fun=snake.turn_left)
+screen.onkeypress(key="Right", fun=snake.turn_right)
+
+
+game_is_on = True
+
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+    snake.move()
+
+    # Detect Collision with food.
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        score.increase_Score()
+        score.update_Score()
+        snake.extend()
+
+    # Detect Collision with Walls
+
+    if (
+        snake.head.xcor() > 290
+        or snake.head.ycor() > 267
+        or snake.head.xcor() < -290
+        or snake.head.ycor() < -280
+    ):
+        game_is_on = False
+        score.highest_score()
+
+    # Detect collision with Tail
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
+            game_is_on = False
+            score.highest_score()
+
+screen.exitonclick()
--- SnakeGameScoreCard.py
+++ SnakeGameScoreCard.py
@@ -9,6 +9,7 @@ class Score(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        self.highscore = 0  # (updated on day24)
         self.penup()
         self.hideturtle()
         self.color("White")
@@ -16,14 +17,23 @@ class Score(Turtle):
         self.update_Score()
 
     def update_Score(self):
-        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT_STYLE)
+        self.clear()
+        self.write(
+            f"Score: {self.score}| Highest Score: {self.highscore}",
+            align=ALIGNMENT,
+            font=FONT_STYLE,
+        )
 
-    def gameOver(self):
-        self.goto(0, 0)
-        self.write("Game Over", align=ALIGNMENT, font=FONT_STYLE)
+    def reset(self):  # added on day24
+        if self.score > self.highscore:
+            self.highscore = self.score
+        self.score = 0
+
+    # def gameOver(self):            # Commented on day24
+    #     self.goto(0, 0)
+    #     self.write("Game Over", align=ALIGNMENT, font=FONT_STYLE)
 
     def increase_Score(self):
         self.score += 1
         self.goto(0, 267)
-        self.clear()
         self.update_Score()
