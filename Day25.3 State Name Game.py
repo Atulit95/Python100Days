f0aa84d60c7ede5504c489b16ccf682884bf209e
Atulit95 <atulitgupta436@gmail.com>
2024-04-08 21:01:18 +0530

    day25

--- Day25.3 State Name Game.py
+++ Day25.3 State Name Game.py
@@ -1,12 +1,40 @@
-IMAGE = "blank_states_img.gif"
 import turtle
+import pandas
+
+IMAGE = "blank_states_img.gif"
 
 screen = turtle.Screen()
 screen.title("State Name Game")
 
 screen.addshape(IMAGE)
 turtle.shape(IMAGE)
+state_data = pandas.read_csv("50_states.csv")
+state_list = state_data["state"].to_list()
 
-answer_state = screen.textinput(title="State name", prompt="Enter the State name: ")
+guessed_state = []
+
+while len(guessed_state) < 50:
+    answer_state = screen.textinput(
+        title=f"{len(guessed_state)}/50 State name", prompt="Enter the State name: "
+    ).title()
+
+    if answer_state in state_list:
+        guessed_state.append(answer_state)
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        state_detail = state_data[state_data.state == answer_state]
+        t.goto(int(state_detail.x), int(state_detail.y))
+        t.write(answer_state)
+    if answer_state == "Exit":
+        missing_state = []
+        for state in state_list:
+            if state not in guessed_state:
+                missing_state.append(state)
+        break
+
+missed_state = pandas.DataFrame(missing_state)
+missed_state.to_csv("missed_state.csv")
+
 
 screen.exitonclick()
