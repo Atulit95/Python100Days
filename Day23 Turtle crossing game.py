3af683085143b02dac1d4322e03f37570092165f
Atulit95 <atulitgupta436@gmail.com>
2024-04-09 13:55:13 +0530

    day23

--- Day23 Turtle crossing game.py
+++ Day23 Turtle crossing game.py
@@ -1,12 +1,20 @@
 from turtle import Screen
-from turtle_crossing_game_turtle_behaviour import turtle_with_behaviour
+from turtle_crossing_game_turtle_behaviour import Player
+import time
+
 
 screen = Screen()
 screen.title("Turtle crossing game")
 screen.setup(width=600, height=600)
 screen.tracer(0)
 
-tur_tle = turtle_with_behaviour()
+game_is_on = True
+tur_tle = Player((0, -280))
+screen.listen()
+screen.onkeypress(key="Up", fun=tur_tle.move_up)
 
-screen.update()
+while game_is_on:
+    time.sleep(0.1)
+    screen.update()
+
 screen.exitonclick()
--- turtle_crossing_game_turtle_behaviour.py
+++ turtle_crossing_game_turtle_behaviour.py
@@ -1,10 +1,15 @@
 from turtle import Turtle
 
 
-class turtle_with_behaviour(Turtle):
-    def __init__(self):
+class Player(Turtle):
+    def __init__(self, position):
         super().__init__()
         self.shape("turtle")
         self.setheading(90)
         self.penup()
-        self.goto(0, -280)
+        self.goto(position)
+
+    def move_up(self):
+        if self.ycor() < 280:
+            new_ycor = self.ycor() + 20
+            self.goto(0, new_ycor)
