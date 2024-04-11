a57c9489be6ce1ab78aa9e451e54b5811956d5dc
Atulit95 <atulitgupta436@gmail.com>
2024-04-11 23:17:45 +0530

    day26

--- Day26.8 NATO alphabet project.py
+++ Day26.8 NATO alphabet project.py
@@ -4,3 +4,18 @@
 
 
 import pandas
+
+data = pandas.read_csv("nato_phonetic_alphabet.csv")
+data_dic=data.to_dict()
+
+user_input = input("Enter Your word: ").upper()
+
+phonetics=[row.letter:row.code for index,row in data.iter]
+
+# phonetics = [value for key, value in data_dic if key in user_input]
+# for letter in user_input:
+#     for key, value in data_dic.iterrows():
+#         if value == letter:
+#             phonetics.append(letter.code)
+
+print(data_dic)
