fa60d048b926433747d913d2a09b611d87cb7554
Atulit95 <atulitgupta436@gmail.com>
2024-04-12 16:25:39 +0530

    day27

--- Day27.4 Button ,Entry in tkinter.py
+++ Day27.4 Button ,Entry in tkinter.py
@@ -12,12 +12,27 @@ my_label.pack()
 # Change label text to any new text on click of a button
 
 
+# def change_label():
+#     my_label.config(text="I am A new Label")
+
+
+# button = Button(text="Here! Click Me.", command=change_label)
+# button.pack()
+
+
+# Entry
+
+
 def change_label():
-    my_label.config(text="I am A new Label")
+    new_text = input.get()
+    my_label.config(text=new_text)
 
 
 button = Button(text="Here! Click Me.", command=change_label)
 button.pack()
+
+input = Entry(width=10)
+input.pack()
 
 
 window.mainloop()
