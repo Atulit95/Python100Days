076bae1540107b374066c98cf08481901e5b8d26
Atulit95 <atulitgupta436@gmail.com>
2024-04-17 23:58:34 +0530

    day29

--- Day29 Password Manager.py
+++ Day29 Password Manager.py
@@ -2,6 +2,85 @@ from tkinter import *
 from tkinter import messagebox
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+# Password Generator Project
+import random
+
+
+def gerated_pass():
+    letters = [
+        "a",
+        "b",
+        "c",
+        "d",
+        "e",
+        "f",
+        "g",
+        "h",
+        "i",
+        "j",
+        "k",
+        "l",
+        "m",
+        "n",
+        "o",
+        "p",
+        "q",
+        "r",
+        "s",
+        "t",
+        "u",
+        "v",
+        "w",
+        "x",
+        "y",
+        "z",
+        "A",
+        "B",
+        "C",
+        "D",
+        "E",
+        "F",
+        "G",
+        "H",
+        "I",
+        "J",
+        "K",
+        "L",
+        "M",
+        "N",
+        "O",
+        "P",
+        "Q",
+        "R",
+        "S",
+        "T",
+        "U",
+        "V",
+        "W",
+        "X",
+        "Y",
+        "Z",
+    ]
+    numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+    symbols = ["!", "#", "$", "%", "&", "(", ")", "*", "+"]
+
+    nr_letters = random.randint(8, 10)
+    nr_symbols = random.randint(2, 4)
+    nr_numbers = random.randint(2, 4)
+
+    password_letters = [random.choice(letters) for _ in range(nr_letters)]
+    password_symbol = [random.choice(symbols) for _ in range(nr_symbols)]
+    password_char = [random.choice(numbers) for _ in range(nr_numbers)]
+
+    password_list = password_letters + password_symbol + password_char
+
+    random.shuffle(password_list)
+
+    password = ""
+    for char in password_list:
+        password += char
+
+    password_entry.insert(0, password)
 
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
@@ -10,10 +89,15 @@ def add_to_file():
     email = email_text_box.get()
     password = password_entry.get()
 
-    is_ok = messagebox.askokcancel(
-        title="Verification",
-        message=f"Would you like to save?\nEmail: {email}\nPassword: {password}",
-    )
+    if len(website) == 0 or len(password) == 0 or len(email) == 0:
+        is_ok = False
+        messagebox.showerror(title="Error", message="Please fill all the details.")
+    else:
+        is_ok = messagebox.askokcancel(
+            title="Verification",
+            message=f"Would you like to save?\nEmail: {email}\nPassword: {password}",
+        )
+
     if is_ok:
         with open("Password_Manager_data.txt", mode="a") as file:
             file.writelines(f"{website} | {email} | {password}\n")
@@ -49,7 +133,7 @@ password_label = Label(text="Password:")
 password_label.grid(column=0, row=3)
 password_entry = Entry(width=30)
 password_entry.grid(column=1, row=3, sticky="W")
-generate_button = Button(text="Generate Password", bg="#FEC7B4")
+generate_button = Button(text="Generate Password", bg="#FEC7B4", command=gerated_pass)
 generate_button.grid(column=2, row=3)
 # add Button
 add_button = Button(
