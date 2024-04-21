13a0f6f7b429531c935fcf24ea457b10aa5a9f88
Atulit95 <atulitgupta436@gmail.com>
2024-04-21 15:37:25 +0530

    day31

--- Day31 FlashCard proram.py
+++ Day31 FlashCard proram.py
@@ -11,15 +11,26 @@ current_card = {}
 
 
 def generate_word():
-    global current_card
+    global current_card, flip_timer
+    window.after_cancel(flip_timer)
     current_card = random.choice(to_learn)
-    canvas.itemconfig(title, text="French")
-    canvas.itemconfig(word, text=current_card["French"])
+    canvas.itemconfig(title, text="French", fill="Black")
+    canvas.itemconfig(word, text=current_card["French"], fill="Black")
+    canvas.itemconfig(card_background, image=front_img)
+    flip_timer = window.after(3000, func=flip_card)
 
 
 def flip_card():
-    canvas.itemconfig(title, text="English")
-    canvas.itemconfig(word, text=current_card["English"])
+    canvas.itemconfig(title, text="English", fill="white")
+    canvas.itemconfig(word, text=current_card["English"], fill="white")
+    canvas.itemconfig(card_background, image=back_img)
+
+
+def is_known():
+    to_learn.remove(current_card)
+    generate_word()
+    data = pandas.DataFrame(to_learn)
+    data.to_csv("flashcard_save.csv")
 
 
 # ------------------------------ UI DESIGN ---------------------------------#
@@ -27,23 +38,21 @@ window = Tk()
 window.title("Flash Card Program")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
-window.after(3000, func=flip_card)
+flip_timer = window.after(3000, func=flip_card)
 
 canvas = Canvas(width=800, height=540, bg=BACKGROUND_COLOR, highlightthickness=0)
-front_img = PhotoImage(file="FlashCard images\card_front.png")
-back_img = PhotoImage(file="FlashCard images\card_back.png")
-card_front_img = canvas.create_image(400, 275, image=front_img)
+front_img = PhotoImage(file="FlashCard_images\card_front.png")
+back_img = PhotoImage(file="FlashCard_images\card_back.png")
+card_background = canvas.create_image(400, 275, image=front_img)
 title = canvas.create_text(400, 150, text="", font=("Aerial", 40, "italic"))
 word = canvas.create_text(400, 300, text="", font=("Aerial", 60, "bold"))
 canvas.grid(row=0, column=0, columnspan=2)
 
-cross_img = PhotoImage(file="FlashCard images\wrong.png")
-cross_button = Button(
-    image=cross_img, border=0, highlightthickness=0, command=generate_word
-)
+cross_img = PhotoImage(file="FlashCard_images\wrong.png")
+cross_button = Button(image=cross_img, border=0, highlightthickness=0, command=is_known)
 cross_button.grid(row=1, column=0)
 
-check_img = PhotoImage(file="FlashCard images\check.png")
+check_img = PhotoImage(file="FlashCard_images\check.png")
 check_button = Button(
     image=check_img, border=0, highlightthickness=0, command=generate_word
 )
