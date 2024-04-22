553779f9fb5e0127377e58b8aa9aceb9db42cc5b
Atulit95 <atulitgupta436@gmail.com>
2024-04-22 16:56:10 +0530

    day34

---
+++ Day34.1 Quizzler App.py
@@ -0,0 +1,20 @@
+from Quizzler_question_model import Question
+from Quizzler_data import question_data
+from Quizzler_quiz_brain import QuizBrain
+
+question_bank = []
+# print(question_data)
+for question in question_data:
+    question_text = question["question"]
+    question_answer = question["correct_answer"]
+    new_question = Question(question_text, question_answer)
+    question_bank.append(new_question)
+
+
+quiz = QuizBrain(question_bank)
+
+while quiz.still_has_questions():
+    quiz.next_question()
+
+# print("You've completed the quiz")
+# print(f"Your final score was: {quiz.score}/{quiz.question_number}")
---
+++ Quizzler_data.py
@@ -0,0 +1,6 @@
+import requests
+
+parameter = {"amount": 10, "category": 18, "type": "boolean"}
+response = requests.get(url="https://opentdb.com/api.php", params=parameter)
+response.raise_for_status()
+question_data = response.json()["results"]
