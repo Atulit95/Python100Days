04b90b54855cf35373b7f944db5705f7a2c6e126
Atulit95 <atulitgupta436@gmail.com>
2024-04-24 20:03:52 +0530

    day36

--- Day36 Stock Trading News Alert.py
+++ Day36 Stock Trading News Alert.py
@@ -4,23 +4,46 @@ import datetime as dt
 STOCK = "TSLA"
 COMPANY_NAME = "Tesla Inc"
 
-date = dt.datetime.now().date()
-formated_date = str(date)
+time = dt.datetime.now()
+day = time.day
+if day < 10:
+    day = "0" + str(day)
+month = time.month
+if month < 10:
+    month = "0" + str(month)
+
+year = time.year
+# print(f"{day}--{month}--{year}")
 
 ## STEP 1: Use https://www.alphavantage.co
 # When STOCK price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
-tesla_parameters = {
+
+tesla_stock_parameters = {
     "function": "TIME_SERIES_DAILY",
     "symbol": "TSLA",
     "outputsize": "compact",
     "apikey": "",
 }
 tesla_stock_response = requests.get(
-    url="https://www.alphavantage.co/query", params=tesla_parameters
+    url="https://www.alphavantage.co/query", params=tesla_stock_parameters
 )
 tesla_stock_response.raise_for_status()
 stock_data = tesla_stock_response.json()
-print(stock_data["Time Series (Daily)"]["2024-04-23"])
+
+yesterday_closing_data = float(
+    stock_data["Time Series (Daily)"][f"{year}-{month}-{day-1}"]["4. close"]
+)
+
+day_before_yesterday_closing_data = float(
+    stock_data["Time Series (Daily)"][f"{year}-{month}-{day-2}"]["4. close"]
+)
+
+difference_in_closing_values = yesterday_closing_data-day_before_yesterday_closing_data
+
+if difference_in_closing_values<0:
+    percent_decrease=
+
+print(difference_in_closing_values)
 
 ## STEP 2: Use https://newsapi.org
 # Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME.
