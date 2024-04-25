c0322e62dd56b70dddd5e067ac282645efac1ae5
Atulit95 <atulitgupta436@gmail.com>
2024-04-25 22:52:10 +0530

    day 37

--- Day37 Habit_Tracker.py
+++ Day37 Habit_Tracker.py
@@ -1,8 +1,12 @@
 import requests
+import datetime as dt
 
 pixela_endpoints = "https://pixe.la/v1/users"
 pixela_graph_endpoint = "https://pixe.la/v1/users/soulknight/graphs"
 TOKEN = "qd3rie02n20enr20f20"
+
+today_date = dt.datetime.now().date().strftime("%Y%m%d")
+# print((today_date))
 
 pixela_user_parameter = {
     "token": TOKEN,
@@ -19,11 +23,17 @@ pixela_graph_parameter = {
     "type": "float",
 }
 
-pixela_graph_header = {"X-USER-TOKEN": TOKEN}
+pixela_records_parameters = {
+    "date": today_date,
+    "quantity": "5.09"
+}
 
-pixela_user_response = requests.post(url=pixela_endpoints, json=pixela_user_parameter)
+pixela_header = {"X-USER-TOKEN": TOKEN}
 
-pixela_graph_response = requests.post(
-    url=pixela_graph_endpoint, json=pixela_graph_parameter, headers=pixela_graph_header
-)
-print(pixela_graph_response.text)
+# pixela_user_response = requests.post(url=pixela_endpoints, json=pixela_user_parameter)
+
+# pixela_graph_response = requests.post(
+#     url=pixela_graph_endpoint, json=pixela_graph_parameter, headers=pixela_header
+# )
+pixela_data_response=requests.post(url=f"{pixela_graph_endpoint}/{pixela_graph_parameter["id"]}",json=pixela_records_parameters,headers=pixela_header)
+print(pixela_data_response.text)
