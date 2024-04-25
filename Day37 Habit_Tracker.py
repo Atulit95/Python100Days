aecaca1a6cbafc0ea5c6ca22d71abc2c66013fe5
Atulit95 <atulitgupta436@gmail.com>
2024-04-25 21:53:22 +0530

    day37

--- Day37 Habit_Tracker.py
+++ Day37 Habit_Tracker.py
@@ -1,12 +1,29 @@
 import requests
 
 pixela_endpoints = "https://pixe.la/v1/users"
+pixela_graph_endpoint = "https://pixe.la/v1/users/soulknight/graphs"
+TOKEN = "qd3rie02n20enr20f20"
 
 pixela_user_parameter = {
-    "token": "qd3rie02n20enr20f20",
+    "token": TOKEN,
     "username": "soulknight",
     "agreeTermsOfService": "yes",
     "notMinor": "yes",
 }
-pixela_response = requests.post(url=pixela_endpoints, json=pixela_user_parameter)
-print(pixela_response.text)
+
+pixela_graph_parameter = {
+    "id": "g01",
+    "name": "HabitTracker",
+    "unit": "Kilometer",
+    "color": "shibafu",
+    "type": "float",
+}
+
+pixela_graph_header = {"X-USER-TOKEN": TOKEN}
+
+pixela_user_response = requests.post(url=pixela_endpoints, json=pixela_user_parameter)
+
+pixela_graph_response = requests.post(
+    url=pixela_graph_endpoint, json=pixela_graph_parameter, headers=pixela_graph_header
+)
+print(pixela_graph_response.text)
