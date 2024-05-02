a1581ba537f2b14b9deda1c534b9400315da2c22
Atulit95 <atulitgupta436@gmail.com>
2024-05-02 14:38:47 +0530

    Day41

--- demo.py
+++ demo.py
@@ -2,15 +2,18 @@ import requests
 
 para = {
     "fly_from": "LON",
-    "fly_to": "NYC",
-    "date_from": "28/04/2024",
-    "date_to": "01/05/2024",
+    "fly_to": "SYD",
+    "date_from": "02/05/2024",
+    "date_to": "04/05/2024",
     "one_for_city": 1,
 }
 
-headers = {"apikey": "n6ROzICxpBr1H0V3LxQ53u27d-7zyW7i"}
-response = requests.get(
-    url="https://api.tequila.kiwi.com/v2/search", params=para, headers=headers
-)
+headers = {
+    "apikey": "n6ROzICxpBr1H0V3LxQ53u27d-7zyW7i",
+    "accept": "application/json"       
+        }
 
-print(response.json()["data"][0]["countryTo"]["name"])
+response = requests.get(
+    url="https://api.tequila.kiwi.com/v2/search", params=para, headers=headers)
+
+print(response.json())
