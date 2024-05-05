f601f67bc2b1a5431a707d763af1bf2f67ea39d5
Atulit95 <atulitgupta436@gmail.com>
2024-05-05 21:46:11 +0530

    day45

--- Day45.1 Scrapping Htmlfiles.py
+++ Day45.1 Scrapping Htmlfiles.py
@@ -1,5 +1,16 @@
 from bs4 import BeautifulSoup
 
-with open("WebsiteToScrape.html") as file:
-    contents=file.read()
-    
+with open("WebsiteToScrape.html",encoding="utf8") as file: # Be sure to mention encoding in case if soup not working
+        contents=file.read()
+
+soup=BeautifulSoup(contents,"html.parser")
+# print(soup.title.string)
+# print(soup)
+
+# to find all tags in html doc.
+all_anchor_tag=soup.find_all(name="a")
+# print(all_anchor_tag)
+
+for tag in all_anchor_tag:
+        # print(tag.getText())        getText() is used to get text within tag 
+    print(tag.get("href"))
