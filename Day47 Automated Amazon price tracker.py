d591d77f2238031fb34c1a80933ff59db65a8950
Atulit95 <atulitgupta436@gmail.com>
2024-05-21 23:11:15 +0530

    day47

--- Day47 Automated Amazon price tracker.py
+++ Day47 Automated Amazon price tracker.py
@@ -2,10 +2,10 @@ import requests
 from bs4 import BeautifulSoup
 import lxml
 
-response=requests.get(url="https://www.amazon.com/dp/B075CYMYK6?psc=1&ref_=cm_sw_r_cp_ud_ct_FM9M699VKHTT47YD50Q6")
+response=requests.get(url="https://www.amazon.com/dp/B075CYMYK6?ref_=cm_sw_r_cp_ud_ct_FM9M699VKHTT47YD50Q6&th=1")
 product_page=response.text
 # print(product_page)
 
 soup=BeautifulSoup(product_page,"lxml")
-price=soup.select(selector="a-price-whole")
+price=soup.select(selector=".a-price-whole")
 print(price)
