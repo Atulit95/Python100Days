6b563159011955697497542260e1a4b271843036
Atulit95 <atulitgupta436@gmail.com>
2024-05-22 14:11:27 +0530

    day32

--- Day32.1 Email Sending.py
+++ Day32.1 Email Sending.py
@@ -1,7 +1,7 @@
 import smtplib
 
-my_email = "atulitgupta57@gmail.com"
-password = "miuvmsytawmhlman"
+my_email = "your_email"
+password = "your_password"
 
 # Used port 587 for SMTP with TLS encryption, which is the standard port for SMTP submission.
 with smtplib.SMTP("smtp.gmail.com", 587) as connection:
@@ -9,6 +9,6 @@ with smtplib.SMTP("smtp.gmail.com", 587) as connection:
     connection.login(user=my_email, password=password)
     connection.sendmail(
         from_addr=my_email,
-        to_addrs="soulknight67@myyahoo.com",
+        to_addrs="reciever's email",
         msg="Subject:sent mail\n\nHello",  # '\n\n' sepaerate message from subject
     )
