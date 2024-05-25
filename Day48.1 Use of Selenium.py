3a18814528c4f05b607c9206266583e40fef2e02
Atulit95 <atulitgupta436@gmail.com>
2024-05-25 23:15:21 +0530

    day48

--- Day48.1 Use of Selenium.py
+++ Day48.1 Use of Selenium.py
@@ -6,10 +6,10 @@ chrome_option.add_experimental_option("detach",True)
 driver=wd.Chrome(chrome_option)
 driver.get("https://www.amazon.com/dp/B075CYMYK6?ref_=cm_sw_r_cp_ud_ct_FM9M699VKHTT47YD50Q6")
 
-price_digits=driver.find_element(By.CLASS_NAME,value="a-price-whole")
-decimal_value=driver.find_element(By.CLASS_NAME,value="a-price-fraction")
-print(price_digits.text)
-print(decimal_value)
+price_digits=driver.find_element(By.CLASS_NAME,value="a-price-whole").text
+decimal_value=driver.find_element(By.CLASS_NAME,value="a-price-fraction").text
+print(f"The price of desired product is {price_digits}.{decimal_value}")
+
 
 driver.close() # it closes single instance of the browser
 # driver.quit()  # It closes all instance of the browser
