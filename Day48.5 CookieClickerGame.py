66098e7567868d704451a1c9a83d094722edcfbe
Atulit95 <atulitgupta436@gmail.com>
2024-05-27 23:15:54 +0530

    day48

--- Day48.5 CookieClickerGame.py
+++ Day48.5 CookieClickerGame.py
@@ -1,6 +1,7 @@
 from selenium import webdriver as wd
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+import time
 
 # Keep browser open after program finishes
 chrome_option=wd.ChromeOptions()
@@ -13,8 +14,21 @@ driver=wd.Chrome(chrome_option)
 driver.get("http://orteil.dashnet.org/experiments/cookie/ ")
 
 # find and click on cookie
-count=1
-# while count!=0:
-driver.find_element(By.ID,"cookie").click()
+count=15
+while count!=0:
+    driver.find_element(By.ID,"cookie").click()
+    # cookie_amount=driver.find_element(By.ID,"money").value_of_css_property(By.CSS_SELECTOR,".grayed")
+    # print(cookie_amount.text)
+    # time.sleep(3)
+    count-=1
+# cookie_amount=driver.find_element(By.ID,"money")
+
+hello
+
+cursor_redeem_value=driver.find_element(By.CSS_SELECTOR,"#buyCursor b").text.split()[-1]
+print(cursor_redeem_value)
+
+
+
 
 # driver.close()
