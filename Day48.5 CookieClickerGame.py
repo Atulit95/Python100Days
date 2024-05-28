a8441c7ea32029706a2a1e48d7cfc206d3ee9882
Atulit95 <atulitgupta436@gmail.com>
2024-05-28 09:33:03 +0530

    day48

--- Day48.5 CookieClickerGame.py
+++ Day48.5 CookieClickerGame.py
@@ -4,6 +4,7 @@ from selenium.webdriver.common.keys import Keys
 import time
 
 def right_pannel_data():
+    '''Checks for the each upgrade cost at runtime and returns the list of upgrade cost.'''
     list1=[]
     list1.append(int(driver.find_element(By.CSS_SELECTOR,"#buyCursor b").text.split()[-1].replace(",","")))
     list1.append(int(driver.find_element(By.CSS_SELECTOR,"#buyGrandma b").text.split()[-1].replace(",","")))
@@ -15,22 +16,25 @@ def right_pannel_data():
     list1.append(int(driver.find_element(By.CSS_SELECTOR,"#buyTime\ machine b").text.split()[-1].replace(",","")))
     return list1
 
-def run_for_3_seconds(): 
+def run_for_5_seconds(): 
     start_time = time.time() 
     while True: 
-        # Perform some operations here 
+        # Finds cookie & runs this line for 5 seconds
         driver.find_element(By.ID,"cookie").click()
-        # For demonstration purposes, we can just keep looping until 3 seconds have passed 
+        # when time runs out we move to right pannel get the max upgrade we can get 
         if time.time() - start_time >= 5:
-            cookie_amount=int(driver.find_element(By.ID,"money").text.replace(",","")) 
+            # finds amount of cookie present in the instance
+            cookie_amount=int(driver.find_element(By.ID,"money").text.replace(",",""))
+            # checks for each upgade value 
             list2=right_pannel_data()
             index=0
             for value in list2:
+                # checks for the max value we can take for upgrade depending upon cookie amount
                 if value<=cookie_amount:
                     index=list2.index(value)
-                    # print(index)
+                    # clicks on the max upgrade available
             driver.find_element(By.XPATH,f"//div[@id='store']/div[{index+1}]").click()
-            run_for_3_seconds()
+            run_for_5_seconds()
 
 # Keep browser open after program finishes
 chrome_option=wd.ChromeOptions()
@@ -42,6 +46,6 @@ driver=wd.Chrome(chrome_option)
 # Navigate to game page
 driver.get("http://orteil.dashnet.org/experiments/cookie/ ")
 
-run_for_3_seconds()
+run_for_5_seconds()
 
 driver.close()
