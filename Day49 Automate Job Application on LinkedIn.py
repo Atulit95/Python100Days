80c95581899ab7db3745b61cc732d2de17c67bf4
Atulit95 <atulitgupta436@gmail.com>
2024-05-30 18:27:17 +0530

    day49

--- Day49 Automate Job Application on LinkedIn.py
+++ Day49 Automate Job Application on LinkedIn.py
@@ -27,13 +27,15 @@ driver.find_element(By.ID,"username").send_keys("gojay78158@jahsec.com")
 driver.find_element(By.ID,"password").send_keys("998877@At")
 driver.find_element(By.XPATH,"//*[@id='organic-div']/form/div[3]/button").click()
 
+#Replaces Search bar for desired job.
 replace(path='//*[@class="jobs-search-box__text-input jobs-search-box__keyboard-text-input jobs-search-box__text-input--with-clear jobs-search-global-typeahead__input"]',text="marketing intern app brewery")
 replace(path='//*[@class="jobs-search-box__text-input jobs-search-box__text-input--with-clear"]',text="Worldwide")
 
-time.sleep(4)
+#Fill details and Submit Your Application to the Company.
 driver.find_element(By.XPATH,'//*[@id="main"]/div/div[2]/div[2]/div/div[2]/div/div[1]/div/div[1]/div/div[1]/div[1]/div[4]/div/div/div/button').click()
 driver.find_element(By.XPATH,'//*[@class=" artdeco-text-input--input"]').send_keys("7975647987")
 driver.find_element(By.XPATH,'//*[@class="artdeco-button artdeco-button--2 artdeco-button--primary ember-view"]').click()
 
+#Ends the Session
 driver.close()
 
