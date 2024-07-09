8efe588eba6b99caad72cdcca7afd96c53abdc90
Atulit95 <atulitgupta436@gmail.com>
2024-07-09 21:42:34 +0530

    day50

--- Day50 Auto Tinder Swiping Bot.py
+++ Day50 Auto Tinder Swiping Bot.py
@@ -1,5 +1,6 @@
 from selenium import webdriver as wd
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 from time import sleep
 
 # Keeep browser open after prograrm finishes
@@ -30,7 +31,22 @@ sleep(2)
 facebook_element=driver.find_element(by=By.XPATH,value='//*[@id="u1146625330"]/div/div[1]/div/div[1]/div/div/div[2]/div[2]/span/div[2]/button')
 facebook_element.click()
 
+#list of window handles to switch between windows
+windows_list=driver.window_handles
+
+#Switching to second window or pop-up window
+second_window=driver.switch_to.window(windows_list[1])
+
 #delay and login detai submission
 sleep(2)
 email_detail = driver.find_element(by=By.XPATH,value='/html/body/div/div[2]/div[1]/form/div/div[1]/div/input')
 email_detail.send_keys("9555943149")
+
+password_detail = driver.find_element(by=By.XPATH,value='/html/body/div/div[2]/div[1]/form/div/div[2]/div/input')
+password_detail.send_keys("998877@At")
+
+#this do the job of submitting detail and complete login process
+password_detail.send_keys(Keys.ENTER)
+
+
+
